82aaf0c3a6d7968e79eef0256ff60e41fade9bb9
Mitch <mitchell.hayen@gmail.com>
2022-04-22 20:11:34 -0500

    Formik form on AddLocation.js screen to add a GeoCache hunt Database is set up in MySQL Created server.js and configured it to communicate with DB Currently running into issue where I'm getting 404 error from front end when trying to send axios.post to backend.

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+const port = 4000;
+const cors = require('cors');
+app.use(cors());
+
+let host = 'localhost';
+let user = 'root';
+let password = 'mysql';
+let database = 'GeoCacheDB';
+
+app.post("/hunts", async function(request, response){
+    try{
+        console.log("Request received in app.post /hunts");
+        let conn = mysql.createConnection({host: host, user: user, password: password, database: database});
+        await conn.connect();
+
+        let sql = "INSERT INTO hunts (huntName, difficulty, latitude, longitude) VALUES ('"
+        +request.body.huntName+"','"
+        +request.body.difficulty+"','"
+        +request.body.latitude+"','"
+        +request.body.longitude+"');";
+
+        await conn.query(sql, function(err, result){
+            if (err){
+                if(err.errno == 1062){
+                    response.send("DUPENAME");}
+                else{
+                    console.log("An error occurred with sql request: "+err)
+                    response.send("ERROR")
+                }
+            }
+            else{
+                console.log("Insert successful.  Contents: "+sql);
+                response.send("SUCCESS");
+            }
+        })
+        conn.end()
+    }catch(error){
+        console.log("An error occurred in path /hunts: "+error);
+        response.send("ERROR");
+    }
+})
+
+app.listen(port, ()=>console.log("server.js listening on port ",port));
